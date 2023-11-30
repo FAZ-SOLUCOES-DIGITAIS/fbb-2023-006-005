f76cbf0356639f5dff8a741a68dd1b6eb2fea12b
leonardo <leonardobrum.dev@gmail.com>
2023-11-30 16:22:39 -0300

    ajustes finalizados tela6

--- src/app/scenes/modulo5/tela6/tela6.controller.js
+++ src/app/scenes/modulo5/tela6/tela6.controller.js
@@ -16,113 +16,15 @@
       Game.data.currentScreen = 5;
     })
 
-    vm.unlock = Game.unlock;
-
-    if(!Game.data.outros.modulo5.tela6){
-      Game.data.outros.modulo5.tela6 = {};
-    }
-
-    if(!vm.game.data.outros.modulo5.tela6.items){
-      vm.game.data.outros.modulo5.tela6.items = [
-        [true, false],//0
-      ]
-      vm.game.data.outros.modulo5.tela6.currentItem = [
-        0,//0
-      ]
-
-      vm.game.data.outros.modulo5.tela6.quiz = [
-        /*{
-          selected: -1,
-          completed: false,
-          answer: 1,
-          points: 0
-        }*/
-      ]
-
-      vm.game.data.outros.modulo5.tela6.texts = [
-
-      ]
-    }
-
-    vm.items = vm.game.data.outros.modulo5.tela6.items;
-    vm.currentItem = vm.game.data.outros.modulo5.tela6.currentItem;
-    vm.quiz = vm.game.data.outros.modulo5.tela6.quiz;
-    vm.texts = vm.game.data.outros.modulo5.tela6.texts;
-
-    vm.select = function(id, item, callback, scroll){
-      console.log(id, item)
-      $timeout(function(){
-        vm.items[id][item] = true;
-        vm.currentItem[id] = item;
-
-        vm.checkAfterSelect(callback, scroll);
-
-      })
-    }
-
-    vm.checkAfterSelect = function(callback, scroll){
-      if(callback){
-        $timeout(function(){
-          callback();
-        })
-      }
-
-      if(vm.items.every(function(_items){
-        return _items.every(function(_item){
-          return _item;
-        })
-      })){
-        Game.finishScreen();
-      }
-
-      if(scroll){
-        $timeout(function(){
-          vm.scrollTo(scroll);
-        },20)
-      }
-    }
-
-
-    vm.selectQuiz = function(id, item, callback, scroll){
-      if(vm.quiz[id].completed) return;
-
-      $timeout(function(){
-        vm.quiz[id].selected = item;
-
-        //vm.checkAfterSelect(callback, scroll);
-      })
-    }
-
-    vm.finishQuiz = function(id, callback, scroll){
-      if(vm.quiz[id].completed) return;
-
-      $timeout(function(){
-        vm.quiz[id].completed = true;
-
-        vm.checkAfterSelect(callback, scroll);
-      })
-    }
-
-    vm.next = function(id){
-      $timeout(function(){
-        vm.select(id, vm.currentItem[id] + 1)
-      })
+    vm.next = function(){
+      Game.finishScreen();
+      Game.next();
     }
 
     vm.prev = function(id){
       $timeout(function(){
         vm.select(id, vm.currentItem[id] - 1)
       })
-    }
-
-    vm.scrollTo = function(id){
-      var element = document.getElementById(id);
-
-      var y = element.getBoundingClientRect().top + window.scrollY;
-      window.scroll({
-        top: y,
-        behavior: 'smooth'
-      });
     }
 
 
